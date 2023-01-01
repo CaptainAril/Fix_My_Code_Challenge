5a4ae1b3a7d841dbd2b4f52e10cd07cd55cec944
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-01 17:13:35 +0100

    Fix: Geometric representation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
+
 class square():
-    
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -24,7 +23,7 @@ class square():
             if i != self.height - 1:
                 square += '\n'
         return square
-       
+
 
 if __name__ == "__main__":
 
