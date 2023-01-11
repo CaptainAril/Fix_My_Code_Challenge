898258607f1dcea4d2621657a0919223d6eda392
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-11 02:09:10 +0100

    perfect square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,15 +16,15 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def Permiter_of_my_square(self):
         """Perimeter of square"""
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """Prints square."""
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.Permiter_of_my_square())
